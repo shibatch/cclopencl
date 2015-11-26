f2225db3679b50681880fef1f7f7acbd88296031
Naoki Shibata <shibatch@users.sourceforge.net>
2015-11-26 18:32:58 +0900

    Update Label8.java

--- Label8.java
+++ Label8.java
@@ -1,7 +1,9 @@
-// This is an example implementation of a connected component labeling algorithm.
-// Written by Naoki Shibata shibatch.sf.net@gmail.com http://ito-lab.naist.jp/~n-sibata/cclarticle/index.xhtml
+// Written by Naoki Shibata shibatch.sf.net@gmail.com 
+// http://ito-lab.naist.jp/~n-sibata/cclarticle/index.xhtml
+
 // This program is in public domain. You can use and modify this code for any purpose without any obligation.
 
+// This is an example implementation of a connected component labeling algorithm proposed in the following paper.
 // Naoki Shibata, Shinya Yamamoto: GPGPU-Assisted Subpixel Tracking Method for Fiducial Markers,
 // Journal of Information Processing, Vol.22(2014), No.1, pp.19-28, 2014-01. DOI:10.2197/ipsjjip.22.19
 
@@ -16,22 +18,20 @@ public class Label8 {
         for(int y=0;y < ih;y++) {
             for(int x=0;x < iw;x++) {
 		int ptr = y * iw + x;
-		if (x == 0 || y == 0 || x >= iw-1 || y >= ih-1) {
-		    fb[0][ptr] = 0;
-		} else {
-		    fb[0][ptr] = (fb[0][ptr] == 0) ? 0 : ptr;
-		}
+		fb[0][ptr] = (fb[0][ptr] == 0) ? -1 : ptr;
             }
         }
     }
 
-    static int CCLSub(int[][] fb, int pass, int ptr, int iw, int ih) {
-	int g = fb[pass-1][ptr];
+    static int CCLSub(int[][] fb, int pass, int x0, int y0, int iw, int ih) {
+	int g = fb[pass-1][y0 * iw + x0];
 
 	for(int y=-1;y<=1;y++) {
+	    if (y + y0 < 0 || y + y0 >= ih) continue;
 	    for(int x=-1;x<=1;x++) {
-		int q = ptr + y*iw + x;
-		if (fb[pass-1][q] != 0 && fb[pass-1][q] < g) g = fb[pass-1][q];
+		if (x + x0 < 0 || x + x0 >= iw) continue;
+		int q = (y + y0)*iw + x + x0;
+		if (fb[pass-1][q] != -1 && fb[pass-1][q] < g) g = fb[pass-1][q];
 	    }
 	}
 
@@ -39,17 +39,17 @@ public class Label8 {
     }
 
     static void propagation(int[][] fb, int pass, int iw, int ih) {
-	for(int y=1;y < ih-1;y++) {
-	    for(int x=1;x < iw-1;x++) {
+	for(int y=0;y < ih;y++) {
+	    for(int x=0;x < iw;x++) {
 		int ptr = y * iw + x;
 
 		fb[pass][ptr] = fb[pass-1][ptr];
 
 		int h = fb[pass-1][ptr];
-		int g = CCLSub(fb, pass, ptr, iw, ih);
+		int g = CCLSub(fb, pass, x, y, iw, ih);
 
-		if (g != 0) {
-		    for(int i=0;i<6;i++) g = fb[pass-1][g];
+		if (g != -1) {
+		    for(int i=0;i<16;i++) g = fb[pass-1][g];
 
 		    fb[pass][h  ] = fb[pass][h  ] < g ? fb[pass][h  ] : g; // !! Atomic, referring result of current pass
 		    fb[pass][ptr] = fb[pass][ptr] < g ? fb[pass][ptr] : g; // !! Atomic
@@ -84,7 +84,7 @@ public class Label8 {
 	BufferedImage outImage = new BufferedImage(iw, ih, BufferedImage.TYPE_3BYTE_BGR);
         for(int y = 0;y < ih;y++) {
             for(int x = 0;x < iw;x++) {
-		outImage.setRGB(x, y, fb[NPASS-1][y * iw + x] == 0 ? 0 : (fb[NPASS-1][y * iw + x]  * 1103515245 + 12345));
+		outImage.setRGB(x, y, fb[NPASS-1][y * iw + x] == -1 ? 0 : (fb[NPASS-1][y * iw + x]  * 1103515245 + 12345));
             }
         }
 	ImageIO.write(outImage, "png", new File("output.png"));
